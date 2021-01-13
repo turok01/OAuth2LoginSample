93e98c03e1e7e138b12b22d7df4ab117248a6c85
turok01 <44950560+turok01@users.noreply.github.com>
2021-01-13 15:27:04 +0300

    Initial commit

---
+++ src/main/java/com/igor/sample/OAuth2LoginSample/OAuth2LoginSampleApplication.java
@@ -0,0 +1,13 @@
+package com.igor.sample.OAuth2LoginSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2LoginSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2LoginSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igor/sample/OAuth2LoginSample/OAuth2LoginSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.sample.OAuth2LoginSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2LoginSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
